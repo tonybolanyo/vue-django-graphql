4136fbf086b558d2837fc118961da3cec49fcabb
Tony G. Bolaño <tonybolanyo@gmail.com>
2019-09-12 18:11:58 +0200

    Create sampleapp and serve vue dist as django statics

--- src/VueDjango/settings.py
+++ src/VueDjango/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'VueDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend', 'dist')  # The compiled html
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'dist', 'static'),   # Vue generated bundles
+]
